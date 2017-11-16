13c53b61fb3d57fa4868053b067ea2778d29b713
Tulikov Konstantin <Konstantin.Tulikov@mirapolis.ru>
2017-11-17 02:32:42 +0300

    fox fox fox

--- production.js
+++ production.js
@@ -20,7 +20,11 @@
       }
     }
 
-    //100vh
+    window.addEventListener("orientationchange", function() {
+      if (navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) {
+        document.documentElement.innerHTML = document.documentElement.innerHTML;
+      }
+    }, false);
 })()
 
 function setValue(el, ddd) {
