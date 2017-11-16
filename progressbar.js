776257863d9fd649f045251bcf955cd26ee83e4f
Tulikov Konstantin <Konstantin.Tulikov@mirapolis.ru>
2017-11-16 23:10:45 +0300

    refactor OOP

--- progressbar.js
+++ progressbar.js
@@ -25,15 +25,67 @@
       HALF_RADIAN = Math.PI/HALF_CIRCLE,
       THREE_QUARTES_CIRCLE = 270,
       STARTING_ANGLE = Math.PI/HALF_CIRCLE * THREE_QUARTES_CIRCLE;
-      //Конструктор прогресс-бара
+
+  //Что то вроде приватных методов класса
+  //Установка настроек прогресс бара
+  function setOptions(options) {
+      return {
+        width: Number(options.width) || 160,
+        height: Number(options.height) || 160,
+        animate: !!options.animate || false,
+        hidden: !!options.hidden || false,
+        lineWidth: options.lineWidth || '10',
+        backgroundColor: options.backgroundColor || "#ddd",
+        frontColor: options.frontColor || "#ffcc00",
+        radius: Number(options.radius) || 70,
+        fpsAnimation: Number(options.fpsAnimation) || 25,
+        value: Number(options.value) || 0.0
+      }
+  }
+  //Попытка получить из DOM элемент
+  function getDOMObject(el) {
+      el = (document.querySelector && document.querySelector(el)) || document.getElementById(el) || document.getElementsByClassName(el);
+      if (!el) {
+        throw('No elements found. Single results expected');
+      } else if (el.length > 1) {
+        throw('Multiply results found. Single results expected');
+      }
+
+      return el;
+  }
+  //Проверка на canvas
+  function getCanvas() {
+      var canvas = document.createElement('canvas');
+
+      if (!canvas.getContext) {
+          throw('No canvas support found.');
+      } else {
+        return canvas;
+      }
+  }
+  // Метод проверки пришедшего элемента. Если приходит строка, пытаемся найти в DOM этот элемент.
+  function convertor(el) {
+      if (!el) {
+        throw('Element expected');
+      } else {
+         if (typeof el === "string" && document) {
+            el = getDOMObject(el);
+         } else if (typeof el !== "object") {
+            throw('Wrong format of element. Object or string required')
+         }
+      }
+
+      return el;
+  }
+  //Конструктор прогресс-бара
   function ProgressCircleBar(el, options) {
-      var canvas =  this.getCanvas(),
+      var canvas =  getCanvas(),
           context = canvas.getContext('2d'),
-          el = this.convertor(el);
+          el = convertor(el);
 
       el.appendChild(canvas);
 
-      this.options = options = this.setOptions(options || {});
+      this.options = options = setOptions(options || {});
 
       canvas.width = options.width;
       canvas.height = options.height;
@@ -110,55 +162,7 @@
          !options.animate && clearInterval(interval);
        }, fps);
     },
-    // Метод проверки пришедшего элемента. Если приходит строка, пытаемся найти в DOM этот элемент.
-    convertor: function(el) {
-      if (!el) {
-        throw('Element expected');
-      } else {
-         if (typeof el === "string" && document) {
-            el = this.getDOMObject(el);
-         } else if (typeof el !== "object") {
-            throw('Wrong format of element. Object or string required')
-         }
-      }
 
-      return el;
-    },
-    //Попытка получить из DOM элемент
-    getDOMObject: function(el) {
-      el = (document.querySelector && document.querySelector(el)) || document.getElementById(el) || document.getElementsByClassName(el);
-      if (!el) {
-        throw('No elements found. Single results expected');
-      } else if (el.length > 1) {
-        throw('Multiply results found. Single results expected');
-      }
-
-      return el;
-    },
-    //Проверка на canvas
-    getCanvas: function() {
-      var canvas = document.createElement('canvas');
-      if (!canvas.getContext) {
-          throw('No canvas support found.');
-      } else {
-        return canvas;
-      }
-    },
-    //Установка настроек прогресс бара
-    setOptions: function(options) {
-      return {
-        width: Number(options.width) || 160,
-        height: Number(options.height) || 160,
-        animate: !!options.animate || false,
-        hidden: !!options.hidden || false,
-        lineWidth: options.lineWidth || '10',
-        backgroundColor: options.backgroundColor || "#ddd",
-        frontColor: options.frontColor || "#ffcc00",
-        radius: Number(options.radius) || 70,
-        fpsAnimation: Number(options.fpsAnimation) || 25,
-        value: Number(options.value) || 0.0
-      }
-    },
     //Включение анимации
     setAnimate: function(status) {
       status && this.animation();
