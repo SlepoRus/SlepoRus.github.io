2684f444911cd6f32d636f9abf77328ede997c60
Tulikov Konstantin <Konstantin.Tulikov@mirapolis.ru>
2017-11-18 19:07:57 +0300

    refactor building

--- production.js
+++ production.js
@@ -8,23 +8,24 @@
       }),
       input = fields_array.filter(function(val) {
         return val.type === "text";
-      })[0];
+      })[0],
+      getNormalVH = function() {
+        document.getElementsByClassName('b-container')[0].style['height'] = document.documentElement.clientHeight + "px";
+      };
       // Заполнение input'а аналогично выставленным значениям в progressbar'e
-    input.value = progress.progressBar.options.value;
-    // Заполнение checkbox'es аналогично выставленным значениям в progressbar'e
-    for (var i = 0; i < checkbox.length; i++) {
-      var name = checkbox[i].name;
+  input.value = progress.progressBar.options.value;
+  // Заполнение checkbox'es аналогично выставленным значениям в progressbar'e
+  for (var i = 0; i < checkbox.length; i++) {
+    var name = checkbox[i].name;
 
-      if (progress) {
-        checkbox[i].checked = !!progress.progressBar[name];
-      }
+    if (progress) {
+      checkbox[i].checked = !!progress.progressBar[name];
     }
-    function getNormalVH() {
-      document.getElementsByClassName('b-container')[0].style['height'] = document.documentElement.clientHeight + "px";
-    }
-    getNormalVH();
-    window.addEventListener('resize', getNormalVH);
-    window.addEventListener('orientationchange', getNormalVH);
+  }
+
+  getNormalVH();
+  window.addEventListener('resize', getNormalVH);
+  window.addEventListener('orientationchange', getNormalVH);
 })()
 function setValue(el, ddd) {
   var targ = document.getElementById(ddd);
